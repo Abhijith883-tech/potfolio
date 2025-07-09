e13c5323a05a25e4e63beb5c1bc7c99f12dbf133
Abhijith883-tech <abhijithr2801@gmail.com>
2025-07-09 12:26:45 +0530

    potfolio

---
+++ main.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('show');
+};
+
+
+// function sendMail() {
+//     let parms={
+//         name:document.getElementById("name").value,
+//         email:document.getElementById("email").value,
+//         subject:document.getElementById("subject").value,
+//         mobile:document.getElementById("mobile").value,
+//         message:document.getElementById("message").value,
+//     }
+//     emailjs.send("service_3o7hehr","template_0mt928m",parms).then(alert("Email Send"))
+// }
+
+function emailSend() {
+    Email.send({
+        Host: "s1.maildns.net",
+        Username: "username",
+        Password: "password",
+        To: 'them@website.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
+
